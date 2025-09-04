95bcc3082463a4960c3a3e070bfd17f4bb7b3312
Alisson-grem <alisson.agp.rj@gmail.com>
2025-09-03 22:33:32 -0300

    por segurança

--- telas/ListarClientes.tsx
+++ telas/ListarClientes.tsx
@@ -1,12 +1,14 @@
+import {useState, useEffect } from 'react';
 import { View, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
+import Cliente from 
 export default function ListarClientes() {
  return (
     <>
         <View style={styles.bloco}>
             <TouchableOpacity>
                 <Text>Cadastrar Novo Cliente</Text>
-                
+
             </TouchableOpacity>
         </View>
 
